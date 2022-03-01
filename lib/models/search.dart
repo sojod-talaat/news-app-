d4de9f44a13c7e3b9488b8ad8a7f77c9e4bf4e0c
sojod-talaat <sojodtalaat990@hotmail.com>
2022-03-01 22:59:13 +0200

    news

--- lib/models/search.dart
+++ lib/models/search.dart
@@ -5,23 +5,21 @@ import 'package:newsapp/cubit/states.dart';
 import 'package:newsapp/shared/component/newsscreen.dart';
 
 class Search extends StatelessWidget {
-    var searchcontroler = TextEditingController();
+  var searchcontroler = TextEditingController();
   @override
   Widget build(BuildContext context) {
-  
     return BlocConsumer<appcubit, NewsStates>(
         listener: (context, state) {},
         builder: (context, state) {
           var list = appcubit.get(context).searchnewss;
-          return Scaffold(           
+          return Scaffold(
               appBar: AppBar(),
               body: Column(
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: 
-                    TextFormField(
-                      controller:searchcontroler,
+                    child: TextFormField(
+                      controller: searchcontroler,
                       onChanged: (value) {
                         appcubit.get(context).searchnews(value);
                       },
@@ -32,24 +30,19 @@ class Search extends StatelessWidget {
                       ),
                     ),
                   ),
-
-                  SizedBox(
-                    width: 50,
-                  ),
                   Expanded(
-                      child: ListView.separated(
-                          itemBuilder: (context, index) =>
-                              news(list[index], context),
-                          separatorBuilder: (context, index) {
-                            return Container(
-                              width: double.infinity,
-                              height: 1.0,
-                              color: Colors.grey[300],
-                            );
-                          },
-                          itemCount: 5),
-                    ),
-                  
+                    child: ListView.separated(
+                        itemBuilder: (context, index) =>
+                            news(list[index], context),
+                        separatorBuilder: (context, index) {
+                          return Container(
+                            width: double.infinity,
+                            height: 1.0,
+                            color: Colors.grey[300],
+                          );
+                        },
+                        itemCount: 5),
+                  ),
                 ],
               )
 
